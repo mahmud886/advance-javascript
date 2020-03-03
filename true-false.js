d43a7659eb0a401881ba44eba83de36f0e2dbbef
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2020-03-03 23:55:40 +0600

    true-false added

---
+++ true-false.js
@@ -0,0 +1,20 @@
+//falsy;
+//false
+//0;
+//""
+//undefined 
+//NaN
+//Truthy
+// Always false
+
+
+// const name = "Solaiman Khan";
+let name = null;
+console.log(name);
+
+if(name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
